7f510f7b7f45b7edbc1a2ba501437d0e7906728b
Anton Olguin <antonolguin1@gmail.com>
2025-03-30 23:37:43 -0600

    Add project files.

---
+++ src/DataStructures/Node.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EP2ArbolGenealogico.src.DataStructures
+{
+    class Node
+    {
+        private string name;
+        private Node left, right;
+    }
+}
