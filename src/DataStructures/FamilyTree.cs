5221134d34671e428298fb926d7e81af47352c76
NuclearGecko74 <antonolguin1@gmail.com>
2025-04-06 04:16:42 -0600

    test git

---
+++ src/DataStructures/FamilyTree.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EP2ArbolGenealogico.src.DataStructures
+{
+    class FamilyTree
+    {
+        private Node root;
+
+        public FamilyTree()
+        {
+            root = null;
+        }
+
+        public FamilyTree(string name)
+        {
+            root = new Node(name);
+        }
+
+        private Node FindPerson(Node current, string name)
+        {
+            if (current == null) return null;
+            if (current.Name == name)
+                return current;
+
+            Node found = FindPerson(current.Father, name);
+            if (found != null) return found;
+
+            return FindPerson(current.Mother, name);
+        }
+    }
+}
--- src/DataStructures/Node.cs
+++ src/DataStructures/Node.cs
@@ -9,6 +9,31 @@ namespace EP2ArbolGenealogico.src.DataStructures
     class Node
     {
         private string name;
-        private Node left, right;
+        private Node dad, mom;
+
+        public Node(string name)
+        {
+            this.name = name;
+            this.dad = null;
+            this.mom = null;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public Node Father
+        {
+            get { return dad; }
+            set { dad = value; }
+        }
+
+        public Node Mother
+        {
+            get { return mom; }
+            set { mom = value; }
+        }
     }
 }
